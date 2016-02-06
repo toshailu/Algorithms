53dd60441e9c977fc35fea74601006e2232681a5
Shailendra Yadav <shailendra.yadav@redknee.com>
2016-02-06 12:36:35 +0530

    Previous implementation was wrong

--- src/com/shailu/sorting/Insertion_Sort.java
+++ src/com/shailu/sorting/Insertion_Sort.java
@@ -3,56 +3,37 @@ package com.shailu.sorting;
 public class Insertion_Sort {
 
 	public static void main(String[] args) {
-		
-		int[] arr1 = {8,2,4,9,3,6};
-        insertion_sort(arr1);
-        
+
+		int[] arr1 = { 8, 2, 4, 9, 3, 6 };
+		insertion_sort(arr1);
+
 	}
 
 	private static void insertion_sort(int[] arr1) {
-		
-		for(int i=0; i< arr1.length; i++)
-		{
-			for(int j=0 ; j<i; j++)
-			{
-				if(arr1[j] > arr1[j+1])
-				{
-					swap(j,arr1);
-					System.out.println("Swapping elements -"+ arr1[j] + "    "+arr1[j+1]);
+
+		for (int i = 1; i < arr1.length; i++) {
+			for (int j = i; j > 0; j--) {
+				if (arr1[j] < arr1[j - 1]) {
+					swap(j, arr1);
 				}
 			}
-			int t=i+1;
-			print("In iteration - "+ t + "    Array is ---",arr1);
 		}
 		print(arr1);
 	}
 
-	
-	
-	
-	private static void print(String string, int[] arr1) {
-		
-		System.out.print(string);
-		for(int i=0; i<arr1.length; i++)
-		{
-			System.out.print(arr1[i] + ", ");
-		}
-		System.out.println();
-	}
-
 	private static void print(int[] input) {
 
-		for(int i=0; i<input.length; i++)
-		{
+		for (int i = 0; i < input.length; i++) {
 			System.out.print(input[i] + ", ");
 		}
 		System.out.println();
 	}
+
 	private static void swap(int j, int[] arr1) {
 
-		int temp=0;
-		temp=arr1[j];
-		arr1[j]=arr1[j+1];
-		arr1[j+1]=temp;
+		int temp = 0;
+		temp = arr1[j];
+		arr1[j] = arr1[j - 1];
+		arr1[j - 1] = temp;
 	}
 }
